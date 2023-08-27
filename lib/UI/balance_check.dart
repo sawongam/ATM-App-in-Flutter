fb881086afaa8fc0a41adbcfa0b240627a8cfe09
Sangam Adhikari <sangamadhikari.61@gmail.com>
2023-08-27 23:25:13 +0545

    balance_check UI finalized

--- lib/UI/balance_check.dart
+++ lib/UI/balance_check.dart
@@ -1,5 +1,5 @@
+import 'package:atmproject/UI/withdraw.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class BalanceCheck extends StatelessWidget {
   const BalanceCheck({super.key});
@@ -19,76 +19,193 @@ class BalanceCheck extends StatelessWidget {
           child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-            // const Spacer(flex: 5),
-            const Text(
-              'ACCOUNT BALANCE',
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-
-            // const Spacer(flex: 7),
-
-            Container(
-              width: 335,
-              height: 150,
-              decoration: BoxDecoration(
-                color: const Color(0xFF232938),
-                borderRadius: BorderRadius.circular(7),
-              ),
-              child: const Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text(
-                    'Account Type',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                    ),
+                const Spacer(flex: 4),
+                const Text(
+                  'ACCOUNT BALANCE',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.w500,
                   ),
-                  Text(
-                    'CHECKING',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                    ),
+                ),
+                const SizedBox(height: 100),
+                const Spacer(),
+                Container(
+                  width: 335,
+                  height: 150,
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF232938),
+                    borderRadius: BorderRadius.circular(7),
                   ),
-                  Spacer(),
-                  Text(
-                    'Balance',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                    ),
+                  child:  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'ACCOUNT TYPE',
+                        style: TextStyle(
+                          color: Color(0xFF999999),
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: -0.24,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        'CHECKING',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          height: 1.29,
+                          letterSpacing: -0.15,
+                        ),
+                      ),
+                      const Spacer(),
+                      const Text(
+                        'BALANCE',
+                        style: TextStyle(
+                          color: Color(0xFF999999),
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: -0.24,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        'Rs. 69.00',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          height: 1.29,
+                          letterSpacing: -0.15,
+                        ),
+                      ),
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const Withdraw()
+                                ),
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Image.asset('assets/images/Share.png'),
+                                const SizedBox(width: 5),
+                                const Text(
+                                  'WITHDRAW',
+                                  style: TextStyle(
+                                    color: Color(0xFF999999),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                    height: 1.25,
+                                    letterSpacing: -0.32,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  Text(
-                    'Rs. 69.00',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                    ),
+                ),
+                const Spacer(),
+                Container(
+                  width: 335,
+                  height: 150,
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF232938),
+                    borderRadius: BorderRadius.circular(7),
                   ),
-                  Spacer(),
-                  Text(
-                    'WITHDRAW',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                    ),
+                  child:  Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        'ACCOUNT TYPE',
+                        style: TextStyle(
+                          color: Color(0xFF999999),
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: -0.24,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        'CHECKING',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          height: 1.29,
+                          letterSpacing: -0.15,
+                        ),
+                      ),
+                      const Spacer(),
+                      const Text(
+                        'BALANCE',
+                        style: TextStyle(
+                          color: Color(0xFF999999),
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                          letterSpacing: -0.24,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      const Text(
+                        'Rs. 69.00',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          height: 1.29,
+                          letterSpacing: -0.15,
+                        ),
+                      ),
+                      const Spacer(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => const Withdraw()
+                                ),
+                              );
+                            },
+                            child: Row(
+                              children: [
+                                Image.asset('assets/images/Share.png'),
+                                const SizedBox(width: 5),
+                                const Text(
+                                  'WITHDRAW',
+                                  style: TextStyle(
+                                    color: Color(0xFF999999),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w400,
+                                    height: 1.25,
+                                    letterSpacing: -0.32,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-
-            // const Spacer(flex: 5),
-
-          ]),
+                ),
+                const Spacer(flex: 5),
+              ]),
         ),
       ),
     );
